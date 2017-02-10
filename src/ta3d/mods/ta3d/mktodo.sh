df4483083ff402e04f2638d27a8c2497898da689
Michael Heasell <michael.heasell@gmail.com>
2017-02-10 22:44:44 +0000

    Delete "mods" content

--- src/ta3d/mods/ta3d/mktodo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "files to create:" > TODO
-for i in `cat files.txt | tr " " "²"`; do
-	test -e "`echo $i | tr "²" " "`" || test -e "`echo $i | tr "²" " " | sed -e s/\.3do$/\.s3o/`" || test -e "`echo $i | tr "²" " " | sed -e s/\.gaf$//`" || test -e "`echo $i | tr "²" " " | sed -e s/\.cob$/\.lua/`" || echo $i >> TODO;
-done
