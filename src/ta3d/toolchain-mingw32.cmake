efe34569b92970833ad1167518e3cd2c743b5477
Michael Heasell <michael.heasell@gmail.com>
2017-02-11 20:23:08 +0000

    Convert cmake files to lowercase

--- src/ta3d/toolchain-mingw32.cmake
+++ src/ta3d/toolchain-mingw32.cmake
@@ -1,12 +1,12 @@
 # the name of the target operating system
-SET(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
+set(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
+set(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc )
+set(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc )
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
