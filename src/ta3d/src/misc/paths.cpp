7a9f2db4b95f14bb3663ace9a05933b408b26fbb
Michael Heasell <michael.heasell@gmail.com>
2017-02-24 22:20:17 +0000

    Replace Yuni Remove with own implementation

--- src/ta3d/src/misc/paths.cpp
+++ src/ta3d/src/misc/paths.cpp
@@ -463,9 +463,85 @@ namespace TA3D
 #endif
 		}
 
+
+
+# ifdef TA3D_PLATFORM_WINDOWS
+
+	bool Remove(const char* path)
+	{
+		WString<true> fsource(path);
+		if (fsource.empty())
+			return false;
+		int cr;
+
+		SHFILEOPSTRUCTW shf;
+		shf.hwnd = NULL;
+
+		shf.wFunc = FO_DELETE;
+		shf.pFrom = fsource.c_str();
+		shf.pTo = fsource.c_str();
+		shf.fFlags = FOF_SILENT | FOF_NOCONFIRMATION | FOF_NOCONFIRMMKDIR | FOF_NOERRORUI;
+
+		cr = SHFileOperationW(&shf);
+		return (!cr);
+	}
+
+# else
+
+
+	namespace // Anonymous namespace
+	{
+
+		bool RmDirRecursiveInternal(const char path[])
+		{
+			DIR* dp;
+			struct dirent* ep;
+			CustomString<2096> buffer;
+			struct stat st;
+
+			if (NULL != (dp = ::opendir(path)))
+			{
+				while (NULL != (ep = ::readdir(dp)))
+				{
+					buffer.clear() << path << Separator << (const char*)ep->d_name;
+					if (0 == ::stat(buffer.c_str(), &st))
+					{
+						if (S_ISDIR(st.st_mode))
+						{
+							if (strcmp(".", (ep->d_name)) != 0 && strcmp("..", (ep->d_name)) != 0)
+							{
+								RmDirRecursiveInternal(buffer.c_str());
+								::rmdir(buffer.c_str());
+							}
+						}
+						else
+							::unlink(buffer.c_str());
+					}
+				}
+				(void)::closedir(dp);
+			}
+			return (0 == rmdir(path));
+		}
+
+	} // anonymous namespace
+
+
+
+
+	bool Remove(const char path[])
+	{
+		if (NULL == path || '\0' == *path)
+			return true;
+		return RmDirRecursiveInternal(path);
+	}
+
+
+# endif
+
+
 		void RemoveDir(const String& p)
 		{
-			Yuni::Core::IO::Directory::Remove(p);
+			Remove(p.c_str());
 		}
 
 		bool MakeDir(const String& p)
