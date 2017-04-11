6ec9ce4647884b2f10cb1a09a3c9e7a852bd33e5
Michael Heasell <michael.heasell@gmail.com>
2017-04-11 20:41:02 +0100

    Fix file name in comment

--- src/ta3d/src/vfs/virtualfile.h
+++ src/ta3d/src/vfs/virtualfile.h
@@ -16,7 +16,7 @@
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA*/
 
 /*
-**  File: realfile.h
+**  File: virtualfile.h
 ** Notes:
 **   Zuzuf:  This module implements a File object for memory buffers
 */
